70e9ed5ab3788c65d0dc493c3b6dd55aba1dd3b8
seed <seed@seedpc>
2020-07-16 14:26:15 -0500

    16/jul/2020-1

--- app/views/frameGenerateCot.py
+++ app/views/frameGenerateCot.py
@@ -47,11 +47,23 @@ class generateCotView(wx.Panel):
         self.labelSeis = wx.StaticText(self, -1, "Email")
         self.labelSiete = wx.StaticText(self, -1, "Duracion de la obra")
         self.labelOcho = wx.StaticText(self, -1, "Cotizacion")
+        self.labelNueve= wx.StaticText(self, -1, "Sub Total Costo Directo(MO + Materiales)")
+        self.labelDiez = wx.StaticText(self, -1, "Gastos Administrativos (11%)")
+        self.labelOnce = wx.StaticText(self, -1, "Imprevistos (4%)")
+        self.labelDoce = wx.StaticText(self, -1, "Utilidad (5%)")
+        self.labelTrece = wx.StaticText(self, -1, "Iva sobre utilidad (19%)")
+        self.labelCatorce = wx.StaticText(self, -1, "Costo Total Directo + Costo Total Indirecto")
+
+        #lines
+        self.line_uno = wx.StaticLine(self, -1, size =(400, 1), style = (wx.LI_VERTICAL ))
+        self.line_dos = wx.StaticLine(self, -1, size = (400, 1), style = (wx.LI_VERTICAL) )
+        self.lineDuno = wx.StaticLine(self, -1, size = (400, 1), style = (wx.LI_VERTICAL) )
+        self.line_tres = wx.StaticLine(self, -1, style = (wx.LI_HORIZONTAL) )
 
         #tables
 
         self.tablaUno = grid.Grid(self, -1)
-        self.tablaDos = grid.Grid(self, -1)
+        self.tablaDos = grid.Grid(self, -1, size=(600,200))
         self.tablaUno.SetRowLabelSize(0)
         self.tablaDos.SetRowLabelSize(0)
         self.createTableActivities()
@@ -79,7 +91,9 @@ class generateCotView(wx.Panel):
         sizerTres.Add(self.buttonTres, 1, wx.CENTRE | wx.ALL, 1 )
 
         sizerDos.Add(sizerTres, 1, wx.EXPAND | wx.ALL, 1)
+        sizerDos.Add(self.line_uno, 0, wx.EXPAND | wx.ALL, 6)
         sizerDos.Add(self.tablaUno, 1,  wx.CENTER | wx.ALL , 1)
+        sizerDos.AddSpacer(20)
 
         sizerCinco.Add(self.labelDos, 1, wx.ALIGN_LEFT | wx.ALL, 1)
         sizerCinco.Add(self.textCtrlUno, 1, wx.ALIGN_LEFT | wx.ALL, 1)
@@ -101,16 +115,26 @@ class generateCotView(wx.Panel):
 
         sizerCuatro.Add(self.labelOcho, 1, wx.ALIGN_LEFT | wx.ALL, 1)
         sizerCuatro.Add(self.tablaDos, 1, wx.CENTER | wx.ALL, 1 )
+        sizerCuatro.Add(self.line_dos, 0, wx.EXPAND | wx.ALL, 5 )
         sizerCuatro.Add(sizerCinco, 1, wx.EXPAND | wx.ALL, 1)
         sizerCuatro.Add(sizerSeis, 1, wx.EXPAND | wx.ALL, 1)
         sizerCuatro.Add(sizerSiete, 1, wx.EXPAND | wx.ALL, 1)
         sizerCuatro.Add(sizerOcho, 1, wx.EXPAND | wx.ALL, 1)
         sizerCuatro.Add(sizerNueve, 1, wx.EXPAND | wx.ALL, 1)
         sizerCuatro.Add(sizerDiez, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.Add(self.lineDuno, 0, wx.EXPAND | wx.ALL, 5 )
+        sizerCuatro.Add(self.labelNueve, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.Add(self.labelDiez, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.Add(self.labelOnce, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.Add(self.labelDoce, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.Add(self.labelTrece, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.Add(self.labelCatorce, 1, wx.EXPAND | wx.ALL, 1)
+        sizerCuatro.AddSpacer(20)
 
-        #-------------------------------------------------
-        sizerUno.Add(sizerDos, 1, wx.EXPAND | wx.ALL, 15)
-        sizerUno.Add(sizerCuatro, 1, wx.EXPAND | wx.ALL, 15)
+        #------------------------------------------------- 
+        sizerUno.Add(sizerDos, 1, wx.EXPAND | wx.ALL, 10)
+        sizerUno.Add(self.line_tres, 0,  wx.EXPAND | wx.ALL, 5)
+        sizerUno.Add(sizerCuatro, 1, wx.EXPAND | wx.ALL, 12)
 
         self.SetSizer(sizerUno)
     
